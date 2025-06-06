bfb9f63b3210487ed0ddbc5d2d70dea364b7950f
shxrox <sharondeva8@gmail.com>
2025-06-06 13:22:44 +0530

    model

---
+++ src/main/java/com/example/crudbackend/Model/Crud.java
@@ -0,0 +1,59 @@
+package com.example.crudbackend.Model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Table
+@Entity
+public class Crud {
+
+    @Id
+    private Long id;
+    private String name;
+    private int age;
+    private String email;
+
+    public Crud() {
+    }
+
+    public Crud(Long id, String name, int age, String email) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
