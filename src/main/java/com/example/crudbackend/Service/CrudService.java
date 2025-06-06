967db022438aa39b29ea6bfff3c389c2e68903dd
shxrox <sharondeva8@gmail.com>
2025-06-06 15:50:00 +0530

    Service

---
+++ src/main/java/com/example/crudbackend/Service/CrudService.java
@@ -0,0 +1,41 @@
+package com.example.crudbackend.Service;
+
+
+import com.example.crudbackend.Model.Crud;
+import com.example.crudbackend.Repository.CrudRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CrudService {
+
+    @Autowired
+    private CrudRepository crudRepository;
+
+    public List<Crud> getCrud() {
+        return crudRepository.findAll();
+    }
+
+    public Crud CrudById(Long id){
+        return crudRepository.findById(id).orElse(null);
+    }
+
+    public Crud saveCrud(Crud crud){
+        return crudRepository.save(crud);
+    }
+
+    public void deleteCrud(Long id){
+         crudRepository.deleteById(id);
+    }
+
+    public Crud updateCrud(Long id, Crud crud){
+        Crud existingCrud = crudRepository.findById(id).orElse(null);
+        existingCrud.setName(crud.getName());
+        existingCrud.setAge(crud.getAge());
+        existingCrud.setEmail(crud.getEmail());
+        return crudRepository.save(existingCrud);
+    }
+
+}
