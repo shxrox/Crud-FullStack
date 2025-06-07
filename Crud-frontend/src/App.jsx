9a2ee856bf75132178e66f763c0608ba71d9a1f5
shxrox <sharondeva8@gmail.com>
2025-06-07 11:47:19 +0530

    front end updating

--- Crud-frontend/src/App.jsx
+++ Crud-frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import CrudApp from './Componet/CrudApp'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
-
+      <CrudApp />
     </>
   )
 }
